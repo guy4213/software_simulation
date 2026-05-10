45b6ffb93c23af303788910766d9dac8b124a511
Skelepap12 <michaelimichael2004@gmail.com>
2026-05-10 21:47:32 +0300

    yes

--- main.c
+++ main.c
@@ -4,52 +4,56 @@
 
 /* --- Data Structures Definitions --- */
 
-typedef enum {
+typedef enum
+{
     NORTH = 'N',
     SOUTH = 'S',
     EAST = 'E',
     WEST = 'W'
 } Direction;
 
-typedef struct {
+typedef struct
+{
     char plateNumber[10];
     int arrivalTime;
     Direction dir;
 } Vehicle;
 
 /* Doubly Linked List Node */
-typedef struct Node {
+typedef struct Node
+{
     Vehicle data;
-    struct Node* prev;
-    struct Node* next;
+    struct Node *prev;
+    struct Node *next;
 } Node;
 
 /* Queue Wrapper for each Lane */
-typedef struct {
-    Node* head; /* Points to the first vehicle (next to leave) */
-    Node* tail; /* Points to the last vehicle (newly arrived) */
+typedef struct
+{
+    Node *head; /* Points to the first vehicle (next to leave) */
+    Node *tail; /* Points to the last vehicle (newly arrived) */
     int count;
 } Queue;
 
-typedef struct {
+typedef struct
+{
     Queue lanes[4];         /* 0: North, 1: South, 2: East, 3: West */
     Direction currentGreen; /* Current direction with green light */
     int greenTicksLeft;
 } Intersection;
 
-
 /* --- Function Prototypes --- */
 
-void initIntersection(Intersection* intersection);
-void enqueue(Queue* q, Vehicle v);
-int dequeue(Queue* q, Vehicle* outVehicle);
+void initIntersection(Intersection *intersection);
+void enqueue(Queue *q, Vehicle v);
+int dequeue(Queue *q, Vehicle *outVehicle);
 int getLaneIndex(Direction dir);
-void printIntersectionState(Intersection* intersection, int currentTick);
-
+void printIntersectionState(Intersection *intersection, int currentTick);
 
 /* --- Main Simulation Skeleton --- */
 
-int main() {
+int main()
+{
     Intersection myIntersection;
     initIntersection(&myIntersection);
 
@@ -57,26 +61,30 @@ int main() {
 
     printf("=== Starting Traffic Simulation ===\n\n");
 
-    for (int tick = 1; tick <= maxTicks; tick++) {
+    for (int tick = 1; tick <= maxTicks; tick++)
+    {
 
         /* 1. Event Generation (Mocking vehicle arrivals) */
-        if (tick == 1) {
-            Vehicle v1 = { "123-ABC", tick, NORTH };
+        if (tick == 1)
+        {
+            Vehicle v1 = {"123-ABC", tick, NORTH};
             enqueue(&myIntersection.lanes[getLaneIndex(NORTH)], v1);
         }
-        if (tick == 2) {
-            Vehicle v2 = { "987-XYZ", tick, NORTH };
+        if (tick == 2)
+        {
+            Vehicle v2 = {"987-XYZ", tick, NORTH};
             enqueue(&myIntersection.lanes[getLaneIndex(NORTH)], v2);
         }
 
         /* 2. Traffic Logic (Processing the green light lane) */
         Vehicle departingVehicle;
-        //todo-fix : currentGreen param needs to have its name changed to better reflect its purpose
+        // todo-fix : currentGreen param needs to have its name changed to better reflect its purpose
         int laneIdx = getLaneIndex(myIntersection.currentGreen);
 
-        if (dequeue(&myIntersection.lanes[laneIdx], &departingVehicle)) {
+        if (dequeue(&myIntersection.lanes[laneIdx], &departingVehicle))
+        {
             printf("[Tick %d] Vehicle %s passed from lane %c.\n",
-                tick, departingVehicle.plateNumber, departingVehicle.dir);
+                   tick, departingVehicle.plateNumber, departingVehicle.dir);
         }
 
         /* 3. Display current state */
@@ -90,11 +98,12 @@ int main() {
     return 0;
 }
 
-
 /* --- Function Implementations (Stubs) --- */
 
-void initIntersection(Intersection* intersection) {
-    for (int i = 0; i < 4; i++) {
+void initIntersection(Intersection *intersection)
+{
+    for (int i = 0; i < 4; i++)
+    {
         intersection->lanes[i].head = NULL;
         intersection->lanes[i].tail = NULL;
         intersection->lanes[i].count = 0;
@@ -104,36 +113,47 @@ void initIntersection(Intersection* intersection) {
 }
 
 /* Helper to map Enum character to array index */
-int getLaneIndex(Direction dir) {
-    switch (dir) {
-    case NORTH: return 0;
-    case SOUTH: return 1;
-    case EAST:  return 2;
-    case WEST:  return 3;
-    default:    return -1;
+int getLaneIndex(Direction dir)
+{
+    switch (dir)
+    {
+    case NORTH:
+        return 0;
+    case SOUTH:
+        return 1;
+    case EAST:
+        return 2;
+    case WEST:
+        return 3;
+    default:
+        return -1;
     }
 }
 
-void enqueue(Queue* q, Vehicle v) {
+void enqueue(Queue *q, Vehicle v)
+{
     /* TODO: Implement Doubly Linked List insertion at tail */
     q->count++; /* Temporary increment to show activity in skeleton */
 }
 
-int dequeue(Queue* q, Vehicle* outVehicle) {
+int dequeue(Queue *q, Vehicle *outVehicle)
+{
     /* TODO: Implement Doubly Linked List removal from head */
-    if (q->count > 0) {
+    if (q->count > 0)
+    {
         q->count--;
         /* Mocking a return for the skeleton */
-        //strcpy_s(outVehicle->plateNumber, "MOCK-1");
-        //outVehicle->dir = NORTH;
-        //return 1;
+        // strcpy_s(outVehicle->plateNumber, "MOCK-1");
+        // outVehicle->dir = NORTH;
+        // return 1;
     }
     return 0;
 }
 
-void printIntersectionState(Intersection* intersection, int currentTick) {
+void printIntersectionState(Intersection *intersection, int currentTick)
+{
     int northIdx = getLaneIndex(NORTH);
     printf("[State at Tick %d] Green Light: %c | Waiting in North: %d\n",
-        currentTick, intersection->currentGreen, intersection->lanes[northIdx].count);
+           currentTick, intersection->currentGreen, intersection->lanes[northIdx].count);
     printf("----------------------------------------\n");
 }
